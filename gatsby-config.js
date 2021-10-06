6e524595e52bdc568a9e9009ce43709a1146782f
steveyuowo <littlepageprogram@163.com>
2021-10-06 22:04:57 +0800

    Update docker scripts

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,48 +1,48 @@
 module.exports = {
-	siteMetadata: {
-		siteUrl: 'https://www.steveyuowo.com',
-		title: 'steveyuowo',
-		author: 'steve yu',
-		email: 'steveyuowo@gmail.com'
-	},
-	plugins: [
-		{
-			resolve: 'gatsby-source-wordpress',
-			options: {
-				url: 'http://steveyuowo.local/graphql'
-			}
-		},
-		{
-			resolve: 'gatsby-plugin-sass',
-			options: {
-				additionalData: `@import "./src/scss/_variables";`
-			}
-		},
-		'gatsby-plugin-image',
-		'gatsby-plugin-react-helmet',
-		'gatsby-plugin-sitemap',
-		{
-			resolve: 'gatsby-plugin-manifest',
-			options: {
-				icon: 'src/assets/profile.png'
-			}
-		},
-		'gatsby-plugin-sharp',
-		'gatsby-transformer-sharp',
-		{
-			resolve: 'gatsby-source-filesystem',
-			options: {
-				name: 'assets',
-				path: './src/assets/'
-			}
-		},
-		{
-			resolve: 'gatsby-plugin-typescript',
-			options: {
-				isTSX: true, // defaults to false
-				jsxPragma: `jsx`, // defaults to "React"
-				allExtensions: true // defaults to false
-			}
-		}
-	]
+  siteMetadata: {
+    siteUrl: 'https://www.steveyuowo.com',
+    title: 'steveyuowo',
+    author: 'steve yu',
+    email: 'steveyuowo@gmail.com'
+  },
+  plugins: [
+    {
+      resolve: 'gatsby-source-wordpress',
+      options: {
+        url: 'http://47.242.132.145:8006/graphql'
+      }
+    },
+    {
+      resolve: 'gatsby-plugin-sass',
+      options: {
+        additionalData: `@import "./src/scss/_variables";`
+      }
+    },
+    'gatsby-plugin-image',
+    'gatsby-plugin-react-helmet',
+    'gatsby-plugin-sitemap',
+    {
+      resolve: 'gatsby-plugin-manifest',
+      options: {
+        icon: 'src/assets/profile.png'
+      }
+    },
+    'gatsby-plugin-sharp',
+    'gatsby-transformer-sharp',
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        name: 'assets',
+        path: './src/assets/'
+      }
+    },
+    {
+      resolve: 'gatsby-plugin-typescript',
+      options: {
+        isTSX: true, // defaults to false
+        jsxPragma: `jsx`, // defaults to "React"
+        allExtensions: true // defaults to false
+      }
+    }
+  ]
 };
---
+++ scripts/run-wordpress-in-docker.sh
@@ -0,0 +1,2 @@
+docker-compose -f docker-compose.wordpress.yml up -d 
+docker-compose -f docker-compose.wordpress.yml down 
